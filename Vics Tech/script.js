2a15d3f9cf85b0b5872d82cda649251f062fa7e7
Victor Mbugua <93434933+VicMbugua@users.noreply.github.com>
2023-04-20 11:35:45 +0300

    Add files via upload

---
+++ Vics Tech/script.js
@@ -0,0 +1,41 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n){
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n){
+    let i;
+    let slides = document.getElementsByClassName("image");
+    if(n > slides.length){
+        slideIndex = 1;
+    }
+    if(n < 1){
+        slideIndex = slides.length;
+    }
+    for(i = 0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
+
+let mybutton = document.getElementById("top-btn");
+window.onscroll = function(){scrollFunction()};
+function scrollFunction(){
+    if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80){
+        mybutton.style.display = "block";
+    }
+    else{
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+let laptopheader = document.getElementById("products-header").innerHTML;
+function boughtMessage(){
+    alert("Successfully bought " + laptopheader);
+}
